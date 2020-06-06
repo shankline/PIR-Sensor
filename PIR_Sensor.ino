ce06e679c499b5237a84ed3b79ed86b0f6088ae7
STELLAMARUH <stellamaruh@gmail.com>
2020-06-06 17:00:08 +0300

    my first work

---
+++ PIR_Sensor.ino
@@ -0,0 +1,28 @@
+int LED = 13;
+int PIR = 2;
+
+
+void setup(){
+  pinMode(LED,OUTPUT);
+  pinMode(PIR,OUTPUT);
+  Serial.begin(9600);
+  
+  }
+  void loop(){
+    if(digitalRead(PIR)==HIGH) {
+      digitalWrite(LED, HIGH);
+      Serial.println("Motion detected!");
+      delay(100);
+      
+      }
+      else{
+        digitalWrite(LED,LOW);
+        Serial.println("Motion stopped");
+        delay(100);
+      }
+    
+    
+    
+    
+    }
+
